411177efeb43e49699d9e044284b3142e0fd44eb
lejiebianlidian <visualstudio365@gmail.com>
2019-06-21 10:17:15 +0800

    初始化数据库

---
+++ EF6WithMiniProfiler/Models/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EF6WithMiniProfiler.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext() : base("name=SqlConnectionString")
+        {
+
+        }
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<BlogDetail> BlogDetails { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Passport> Passports { get; set; }
+        public virtual DbSet<PassportStamp> PassportStamps { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+
+
+    }
+}
---
+++ EF6WithMiniProfiler/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EF6WithMiniProfiler.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [InverseProperty("CreatedBy")]
+        public List<Post> PostsWritten { get; set; }
+
+        [InverseProperty("UpdatedBy")]
+        public List<Post> PostsUpdated { get; set; }
+
+
+    }
+}
